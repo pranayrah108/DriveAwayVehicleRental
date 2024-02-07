d315cc5074493801cbebf58dd51fa76ec45e6843
pranay <pranayrahangdale23@gmail.com>
2024-02-07 23:10:57 +0530

    User Service updated

--- driveawaybackend/src/main/java/com/app/service/UserService.java
+++ driveawaybackend/src/main/java/com/app/service/UserService.java
@@ -1,32 +1,53 @@
 package com.app.service;
 
+import java.util.List;
+
+import com.app.entities.Booking;
+import com.app.entities.User;
+import com.app.entities.Vehicle;
+
 public interface UserService {
+	
+	//newUserRegistration
+	User newUserRegistration(User newUser);
+	
+	//user login
+	
+	
+	//for user vehicle selection
+	//List<Vehicle> getVehicleById(Long vehicleReqId);
+	
+	//insertUserBooking
+	Booking insertUserBooking(Booking bookingObj);
+	
+	//showAllUserBookings
+	//List<Booking> showAlluserBooking(String userEmail);
+	
+	//deleteUserBooking
+	String deleteUserBooking(Long bookingId);
+	
+	//showAllUserBookingsById
+
+	
+	//findByUserEmail
+	
+	
+	//update password
+	
+	
+	//paymentInfo
+	
+	
+	//acceptpayment
+	
+	
+	//-------------------------------------------------
 //	
 //	List<Employee> getAllEmps();
 //	Employee addEmpDetails(Employee newEmp);
 //	String deleteEmpDetails(Long emoId);
 //	Employee getEmpDetails(Long empId);
 //	Employee updateEmpDetails(Employee emp);
-//	
-	
-	//user login
-	
-	//for user vehicle selection
-	
-	//booking
-	
-	//showAllBooking
-	
-	//deleteBooking
-	
-	//showAllUserBookingById
-	
-	//findByUserEmail
-	
-	//update password
-	
-	//paymentInfo
-	
-	////acceptpayment
+//
 
 }
--- driveawaybackend/src/main/java/com/app/service/UserServiceImpl.java
+++ driveawaybackend/src/main/java/com/app/service/UserServiceImpl.java
@@ -1,5 +1,58 @@
 package com.app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.app.dao.BookingDao;
+import com.app.dao.UserDao;
+import com.app.dao.VehicleDao;
+import com.app.entities.Booking;
+import com.app.entities.User;
+import com.app.entities.Vehicle;
+
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private VehicleDao vehicleDao;
+	
+	@Autowired
+	private BookingDao bookingDao;
+
+	@Override
+	public User newUserRegistration(User newUser) {
+		
+		return userDao.save(newUser);
+	}
+
+//	@Override
+//	public List<Vehicle> getVehicleById(Long vehicleReqId) {
+//		
+//		return vehicleDao.findBy;
+//	}
+
+	@Override
+	public Booking insertUserBooking(Booking bookingObj) {
+		
+		return bookingDao.save(bookingObj);
+	}
+
+//	@Override
+//	public List<Booking> showAlluserBooking(String userEmail) {
+//		
+//		return null;
+//	}
+
+	@Override
+	public String deleteUserBooking(Long bookingId) {
+		if(bookingDao.existsById(bookingId)) {
+			bookingDao.deleteById(bookingId);
+			return "Deketed userBooking details";
+		}
+		return "invalidUserBookingId";
+	}
 
 }
