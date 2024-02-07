324097d14c9de018d690ce3b616d779c130024d0
pranay <pranayrahangdale23@gmail.com>
2024-02-07 23:57:58 +0530

    Controller updated

--- driveawaybackend/src/main/java/com/app/controller/AdminController.java
+++ driveawaybackend/src/main/java/com/app/controller/AdminController.java
@@ -1,5 +1,20 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.service.AdminService;
+import com.app.service.OwnerService;
+
+@RestController
 public class AdminController {
+	
+	@Autowired
+	AdminService adminService;
+	
+	@Autowired
+	OwnerService ownerService;
+	
+	
 
 }
