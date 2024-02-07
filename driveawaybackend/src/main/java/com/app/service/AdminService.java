ed6e1aa7b34292ec9c0f659e002e8db919f12bc5
pranay <pranayrahangdale23@gmail.com>
2024-02-07 18:43:08 +0530

    service layer added

--- driveawaybackend/src/main/java/com/app/service/AdminService.java
+++ driveawaybackend/src/main/java/com/app/service/AdminService.java
@@ -1,5 +1,26 @@
 package com.app.service;
 
+import java.util.List;
+
+import com.app.entities.Admin;
+import com.app.entities.Owner;
+import com.app.entities.User;
+import com.app.entities.Vehicle;
+
 public interface AdminService {
+	
+	//admin login
+	List<Admin> adminlogin(String userName);
+	
+	//listing all users
+	List<User> showAllUsers();
+	
+	//listing All Owners
+	List<Owner> showAllOwners();
+	
+	//listing All vehicles
+	List<Vehicle> showAllVehicles();
+	
+	
 
 }
--- driveawaybackend/src/main/java/com/app/service/AdminServiceImpl.java
+++ driveawaybackend/src/main/java/com/app/service/AdminServiceImpl.java
@@ -1,5 +1,52 @@
 package com.app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.app.dao.AdminDao;
+import com.app.dao.OwnerDao;
+import com.app.dao.UserDao;
+import com.app.dao.VehicleDao;
+import com.app.entities.Admin;
+import com.app.entities.Owner;
+import com.app.entities.User;
+import com.app.entities.Vehicle;
+
 public class AdminServiceImpl implements AdminService {
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private AdminDao adminDao;
+	
+	@Autowired
+	private VehicleDao vehicleDao;
+	
+	@Autowired
+	private OwnerDao ownerDao;
+	
+	
+
+	@Override
+	public List<Admin> adminlogin(String userName) {
+		
+		return adminDao.findBy;
+	}
+
+	@Override
+	public List<User> showAllUsers() {
+		return userDao.findAll();
+	}
+
+	@Override
+	public List<Owner> showAllOwners() {
+		return ownerDao.findAll();
+	}
+
+	@Override
+	public List<Vehicle> showAllVehicles() {
+		return vehicleDao.findAll();
+	}
 
 }
