fb95ef87920f9780e21e2c013052580d5e31b5b2
pranay <pranayrahangdale23@gmail.com>
2024-02-07 18:58:22 +0530

    Dao layer added

--- driveawaybackend/src/main/java/com/app/dao/AdminDao.java
+++ driveawaybackend/src/main/java/com/app/dao/AdminDao.java
@@ -1,9 +1,17 @@
 package com.app.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.app.entities.Admin;
 
 public interface AdminDao extends JpaRepository<Admin, Long> {
+	
+//	// for admin login
+//	@Query("select admin from Admin where admin.userName=:userName")
+//	List<Admin> findByUserName(@Param("userName")String userName);
 
 }
